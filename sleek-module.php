ebb3325de34fa0886530475cecb72dee6cd85a9b
AL <powerbuoy@gmail.com>
2019-10-02 03:57:43 +0200

    start

--- sleek-module.php
+++ sleek-module.php
@@ -1,6 +1,57 @@
 <?php
 namespace Sleek\Modules;
 
-class Module {
+abstract class Module {
+	public function __construct () {
 
+	}
+
+	public function created () {
+
+	}
+
+	public function get_field ($name) {
+
+	}
+
+	public function fields () {
+		return [];
+	}
+
+	public function data () {
+		return [];
+	}
+
+	public function render ($template = null, $fields = []) {
+		$inflector = \ICanBoogie\Inflector::get('en');
+
+		# Work out path to template
+		$template = $template ?? apply_filters('sleek_modules_default_template', 'template'); # Default to template.php
+		$modulesPath = apply_filters('sleek_modules_path', '/modules/');
+		$className = (new \ReflectionClass($this))->getShortName(); # https://coderwall.com/p/cpxxxw/php-get-class-name-without-namespace
+		$snakeName = $inflector->underscore($className);
+		$moduleName = str_replace('_', '-', $snakeName);
+		$templatePath = locate_template("$modulesPath$moduleName/$template.php");
+
+		# We found a template to render the module
+		if ($template) {
+			# Get field defaults
+			$defaultFields = $this->fields();
+
+			# Passed in fields is ACF post_id
+			if (!is_array($fields)) {
+				$fields = get_field($snakeName, $fields);
+			}
+
+			# Merge passed in with default
+			foreach ($defaultFields as $field) {
+				if (isset($field['name']) and !isset($fields[$field['name']])) {
+					$fields[$field['name']] = $field['default_value'] ?? null;
+				}
+			}
+
+			# Include template - pass in fields and data
+			\Sleek\Utils\get_template_part("$modulesPath$moduleName/$template", null, array_merge($this->data(), $fields));
+		}
+	}
 }
--- sleek-modules.php
+++ sleek-modules.php
@@ -1,2 +1,63 @@
 <?php
 namespace Sleek\Modules;
+
+#############################
+# Get array of file meta data
+# about module files
+function get_file_meta () {
+	$path = get_stylesheet_directory() . apply_filters('sleek_modules_path', '/modules/') . '**/module.php';
+	$inflector = \ICanBoogie\Inflector::get('en');
+	$files = [];
+
+	foreach (glob($path) as $file) {
+		$pathinfo = pathinfo($file);
+		$basename = basename($pathinfo['dirname']);
+		$snakeName = $inflector->underscore($basename);
+		$className = $inflector->camelize($basename);
+		$label = $inflector->titleize($basename);
+		$labelPlural = $inflector->pluralize($label);
+		$slug = str_replace('_', '-', $snakeName);
+
+		$files[] = (object) [
+			'pathinfo' => $pathinfo,
+			'filename' => $pathinfo['filename'],
+			'snakeName' => $snakeName,
+			'className' => $className,
+			'label' => $label,
+			'labelPlural' => $labelPlural,
+			'slug' => $slug,
+			'path' => $file
+		];
+	}
+
+	return $files;
+}
+
+####################
+# Create all modules
+if ($files = get_file_meta()) {
+	foreach ($files as $file) {
+		# Include the class
+		require_once $file->path;
+
+		# Create instance of class
+		$fullClassName = "Sleek\Modules\\$file->className";
+
+		$obj = new $fullClassName;
+
+		# Run callback
+		$obj->created();
+	}
+}
+
+######################
+# Render single module
+function render ($name, $args = []) {
+	$inflector = \ICanBoogie\Inflector::get('en');
+	$className = $inflector->camelize($name);
+	$fullClassName = "Sleek\Modules\\$className";
+
+	$obj = new $fullClassName;
+
+	$obj->render();
+}
