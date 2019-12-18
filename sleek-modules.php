05328f14dc7a89e5c0f538af53e16ff2c8117ac3
powerbuoy <powerbuoy@gmail.com>
2019-12-18 11:29:16 +0100

    Started on something

--- sleek-modules.php
+++ sleek-modules.php
@@ -205,7 +205,245 @@ function get_module_fields (array $modules, $key, $layout = 'normal') {
 
 ######################
 # Render dummy modules
-# TODO
 function render_dummies ($modules) {
+	foreach ($modules as $module) {
+		$className = \Sleek\Utils\convert_case($module, 'pascal');
+		$fullClassName = "Sleek\Modules\\$className";
+		$templates = get_module_templates($module);
+		$fields = (new $fullClassName)->fields();
 
+		foreach ($templates as $template) {
+			$template = $template['filename'];
+			$data = [];
+
+			foreach ($fields as $field) {
+				$data[$field['name']] = render_dummies_apply_filters(null, $module, $template, $field);
+			}
+
+			render($module, $data, $template);
+		}
+	}
 }
+
+function render_dummies_apply_filters ($value, $module, $template, $field) {
+	$value = apply_filters('sleek_get_dummy_field/?type=' . $field['type'] . '&name=' . $field['name'] . '&module=' . $module, null, $module, $template, $field) ??
+			apply_filters('sleek_get_dummy_field/?type=' . $field['type'] . '&name=' . $field['name'], null, $module, $template, $field) ??
+			apply_filters('sleek_get_dummy_field/?type=' . $field['type'], null, $module, $template, $field);
+
+	return	$value;
+}
+
+#######
+# Basic
+# Text Title
+# TODO: Check prepend/append
+add_filter('sleek_get_dummy_field/?type=text&name=title', function ($value, $module, $template, $field) {
+	return \Sleek\Utils\convert_case($module, 'title') . ': ' . $template;
+}, 10, 4);
+
+# Text
+# TODO: Check prepend/append
+add_filter('sleek_get_dummy_field/?type=text', function ($value, $module, $template, $field) {
+	return 'Lorem ipsum dolor sit amet.';
+}, 10, 4);
+
+# Textarea
+# TODO: Check maxlength
+add_filter('sleek_get_dummy_field/?type=textarea', function ($value, $module, $template, $field) {
+	return 'Lorem ipsum dolor sit amet. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.';
+}, 10, 4);
+
+# Number
+# TODO: Check min/max + prepend/append
+add_filter('sleek_get_dummy_field/?type=number', function ($value, $module, $template, $field) {
+	return rand(0, 999);
+}, 10, 4);
+
+# Range
+# TODO...
+add_filter('sleek_get_dummy_field/?type=range', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Email
+# TODO...
+add_filter('sleek_get_dummy_field/?type=email', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# URL
+# TODO...
+add_filter('sleek_get_dummy_field/?type=url', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Password
+# TODO...
+add_filter('sleek_get_dummy_field/?type=password', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+#########
+# Content
+# Image
+add_filter('sleek_get_dummy_field/?type=image', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# File
+add_filter('sleek_get_dummy_field/?type=file', function ($value, $module, $template, $field) {
+	$file = null;
+
+	# TODO: Limit to $field['mime_types'] if set
+	$rows = get_posts([
+		'post_type' => 'attachment',
+		'numberposts' => -1
+	]);
+
+	if ($rows) {
+		if (isset($field['return_format']) and $field['return_format'] === 'id') {
+			return $rows[array_rand($rows)]->ID;
+		}
+		elseif (isset($field['return_format']) and $field['return_format'] === 'url') {
+			# TODO: Return URL
+		}
+		# NOTE: Default to array - is that correct?
+		else {
+			return acf_get_attachment($rows[array_rand($rows)]->ID);
+		}
+	}
+
+	return $file;
+}, 10, 4);
+
+# WYSIWYG
+add_filter('sleek_get_dummy_field/?type=wysiwyg', function ($value, $module, $template, $field) {
+	return '
+		<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+		<p>Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+	';
+}, 10, 4);
+
+# oEmbed
+# TODO...
+add_filter('sleek_get_dummy_field/?type=oembed', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Gallery
+# TODO...
+add_filter('sleek_get_dummy_field/?type=gallery', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+########
+# Choice
+# Select
+# TODO...
+add_filter('sleek_get_dummy_field/?type=select', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Radio button
+# TODO...
+add_filter('sleek_get_dummy_field/?type=radio', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Button group
+# TODO...
+add_filter('sleek_get_dummy_field/?type=button_group', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# True/false
+# TODO...
+add_filter('sleek_get_dummy_field/?type=true_false', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+############
+# Relational
+# Link
+# TODO...
+add_filter('sleek_get_dummy_field/?type=link', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Post Object
+# TODO...
+add_filter('sleek_get_dummy_field/?type=post_object', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Page Link
+# TODO...
+add_filter('sleek_get_dummy_field/?type=page_link', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Relationship
+# TODO...
+add_filter('sleek_get_dummy_field/?type=relationship', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Taxonomy
+# TODO...
+add_filter('sleek_get_dummy_field/?type=taxonomy', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# User
+# TODO...
+add_filter('sleek_get_dummy_field/?type=user', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+########
+# jQuery
+# Google Map
+# TODO...
+add_filter('sleek_get_dummy_field/?type=google_map', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Date Picker
+# TODO...
+add_filter('sleek_get_dummy_field/?type=date_picker', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Date Time Picker
+# TODO...
+add_filter('sleek_get_dummy_field/?type=datetime_picker', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+# Color Picker
+# TODO...
+add_filter('sleek_get_dummy_field/?type=color_picker', function ($value, $module, $template, $field) {
+	return null;
+}, 10, 4);
+
+########
+# Layout
+# Repeater
+add_filter('sleek_get_dummy_field/?type=repeater', function ($value, $module, $template, $field) {
+	$subFields = [];
+
+	# Random number of repeats
+	for ($i = 0; $i < rand(1, 12); $i++) {
+		$subField = [];
+
+		foreach ($field['sub_fields'] as $sField) {
+			$subField[$sField['name']] = render_dummies_apply_filters(null, $module, $template, $sField);
+		}
+
+		$subFields[] = $subField;
+	}
+
+	return $subFields;
+}, 10, 4);
+
+# TODO: Message, Accordion, Tab, Group, Flexible Content, Clone
