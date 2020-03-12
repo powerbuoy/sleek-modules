0e763adf1ad538bc2dea1d947441643576d78aae
powerbuoy <powerbuoy@gmail.com>
2020-03-13 00:11:23 +0100

    Refactored inline-edit to use iframe

---
+++ inline-edit-form.php
@@ -0,0 +1,94 @@
+<?php acf_form_head() ?>
+
+<!DOCTYPE html>
+
+<html <?php language_attributes() ?> <?php body_class() ?>>
+
+	<head>
+
+		<?php wp_head() ?>
+
+		<style>
+			body {
+				color: #444;
+				font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+				font-size: 13px;
+				line-height: 1.4em;
+				margin: 0;
+			}
+
+			/* This style isn't added by acf_form_head() */
+			.screen-reader-text {
+				display: none;
+			}
+
+			/* Util class */
+			.sleek-modules-inline-edit-hide {
+				display: none;
+			}
+
+			/* Remove first field (the flex field) padding */
+			form.acf-form > .acf-fields > .acf-field {
+				padding: 0;
+				border: 0;
+			}
+
+			/* Never collapse layouts */
+			.acf-flexible-content .layout.-collapsed > .acf-fields,
+			.acf-flexible-content .layout.-collapsed > .acf-table {
+				display: block;
+			}
+
+			/* Remove flex field actions */
+			.acf-flexible-content > .acf-actions {
+				display: none;
+			}
+
+			/* Remove border from layouts */
+			.acf-flexible-content .values .layout {
+				margin: 0;
+				border: 0;
+				display: none;
+			}
+
+			.acf-flexible-content .values .layout[data-id="row-<?php echo $wp_query->query_vars['sleek_modules_inline_edit_index'] ?>"] {
+				display: block;
+			}
+
+			/* Hide layout handle and controls */
+			.acf-flexible-content .values .layout .acf-fc-layout-handle,
+			.acf-flexible-content .values .layout .acf-fc-layout-controls {
+				display: none;
+			}
+
+			/* Remove side padding and border from inner fields */
+			.acf-flexible-content .values .layout .acf-fields > .acf-field {
+				padding-left: 0;
+				padding-right: 0;
+				border: 0;
+			}
+		</style>
+
+	</head>
+
+	<body>
+
+		<?php
+			acf_form([
+				'id' => 'acf-form-' . $wp_query->query_vars['sleek_modules_inline_edit_area'] . '-' . $wp_query->query_vars['sleek_modules_inline_edit_post_id'],
+				'fields' => [$wp_query->query_vars['sleek_modules_inline_edit_area']],
+				'post_id' => $wp_query->query_vars['sleek_modules_inline_edit_post_id']
+			]);
+		?>
+
+		<?php wp_footer() ?>
+
+		<?php if (isset($_GET['updated'])) : ?>
+			<script>
+				window.parent.postMessage({sleekModulesInlineEditUpdated: true}, '<?php echo home_url() ?>');
+			</script>
+		<?php endif ?>
+
+	</body>
+
+</html>
--- inline-edit.php
+++ inline-edit.php
@@ -1,131 +1,82 @@
 <?php
 namespace Sleek\Modules;
 
-# Render ACF form head stuff like CSS/JS etc
-add_action('get_header', function () {
+# Add rewrite rule for our inline edit form
+add_action('init', function () {
 	if (get_theme_support('sleek/modules/inline_edit')) {
-		if (current_user_can('edit_posts') and !is_admin()) {
-			acf_form_head();
-		}
+		add_filter('query_vars', function ($vars) {
+			$vars[] = 'sleek_modules_inline_edit_area';
+			$vars[] = 'sleek_modules_inline_edit_post_id';
+			$vars[] = 'sleek_modules_inline_edit_index';
+
+			return $vars;
+		});
+
+		add_rewrite_rule(
+			'^sleek-modules-inline-edit/([^/]+)/([^/]+)/([^/]+)/?$',
+			'index.php?sleek_modules_inline_edit_area=$matches[1]&sleek_modules_inline_edit_post_id=$matches[2]&sleek_modules_inline_edit_index=$matches[3]',
+			'top'
+		);
+
+		add_action('template_redirect', function () {
+			global $wp_query;
+
+			if (isset($wp_query->query_vars['sleek_modules_inline_edit_area'])) {
+				# Enable jQuery
+				remove_theme_support('sleek/disable_jquery');
+
+				# Disable sleek styling
+				add_action('wp_enqueue_scripts', function () {
+					wp_dequeue_style('sleek');
+					wp_dequeue_script('sleek');
+					wp_dequeue_script('sleek_google_maps');
+				}, 99);
+
+				include __DIR__ . '/inline-edit-form.php';
+				exit;
+			}
+		});
 	}
 });
 
 add_action('after_setup_theme', function () {
-	if (get_theme_support('sleek/modules/inline_edit')) {
-		if (current_user_can('edit_posts') and !is_admin()) {
-			# jQuery is required for acf form...
-			remove_theme_support('sleek/disable_jquery');
+	if (get_theme_support('sleek/modules/inline_edit') and current_user_can('edit_posts') and !is_admin()) {
+		add_action('wp_head', function () {
+			?>
+			<style>
+				.sleek-modules-inline-edit-iframe {
+					border: 0;
+					width: 100%;
+					height: 70vh;
+				}
+			</style>
+			<?php
+		});
 
-			# Render the ACF form above each flexible module field
-			add_action('sleek/modules/pre_render_flexible', function ($where, $id) {
-				?>
-				<div class="dialog" id="dialog-edit-modules-<?php echo $where ?>-<?php echo $id ?>">
-					<?php
-						acf_form([
-							'id' => 'acf-form-' . $where . '-' . $id,
-							'fields' => [$where],
-							'post_id' => $id
-						]);
-					?>
-				</div>
-				<?php
-			}, 10, 2);
-
-			# Render an edit button above each module
-			add_action('sleek/modules/pre_render_flexible_module', function ($where, $id, $module, $data, $index) {
-				?>
-				<nav class="sleek-modules-inline-edit">
-					<a href="#dialog-edit-modules-<?php echo $where ?>-<?php echo $id ?>"
-						class="sleek-modules-inline-edit-module"
-						data-dialog-data='<?php echo json_encode(['where' => $where, 'id' => $id, 'module' => $module, 'index' => $index]) ?>'>
-						<?php _e('Edit Module', 'sleek') ?>
-					</a>
-				</nav>
-				<?php
-			}, 10, 5);
-
-			# Clean up the acf form
-			add_action('wp_head', function () {
-				?>
-				<style>
-					/* This style isn't added by acf_form_head() */
-					.screen-reader-text {
-						display: none;
+		add_action('wp_footer', function () {
+			?>
+			<script>
+				window.addEventListener('message', function (e) {
+					if (e.origin === '<?php echo home_url() ?>' && e.data && e.data.sleekModulesInlineEditUpdated === true) {
+						window.location.reload();
 					}
+				});
+			</script>
+			<?php
+		});
 
-					/* Hide messages */
-					div.dialog[id^="dialog-edit-modules-"] #message {
-						display: none;
-					}
+		add_action('sleek/modules/pre_render_flexible_module', function ($where, $id, $module, $data, $index) {
+			?>
+			<div class="dialog dialog--large" id="dialog-sleek-modules-inline-edit-<?php echo $where ?>-<?php echo $id ?>-<?php echo $index ?>">
+				<iframe src="<?php echo home_url("/sleek-modules-inline-edit/$where/$id/$index/") ?>" class="sleek-modules-inline-edit-iframe"></iframe>
+			</div>
 
-					/* Util class */
-					div.dialog[id^="dialog-edit-modules-"] .sleek-modules-inline-edit-hide {
-						display: none;
-					}
-
-					/* Remove first field (the flex field) padding */
-					div.dialog[id^="dialog-edit-modules-"] form.acf-form > .acf-fields > .acf-field {
-						padding: 0;
-						border: 0;
-					}
-
-					/* Never collapse layouts */
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content .layout.-collapsed > .acf-fields,
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content .layout.-collapsed > .acf-table {
-						display: block;
-					}
-
-					/* Remove flex field actions */
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content > .acf-actions {
-						display: none;
-					}
-
-					/* Remove border from layouts */
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content .values .layout {
-						margin: 0;
-						border: 0;
-					}
-
-					/* Hide layout handle and controls */
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content .values .layout .acf-fc-layout-handle,
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content .values .layout .acf-fc-layout-controls {
-						display: none;
-					}
-
-					/* Remove side padding and border from inner fields */
-					div.dialog[id^="dialog-edit-modules-"] .acf-flexible-content .values .layout .acf-fields > .acf-field {
-						padding-left: 0;
-						padding-right: 0;
-						border: 0;
-					}
-				</style>
-				<?php
-			});
-
-			# Listen to dialog open to re-arrange the editor
-			add_action('wp_footer', function () {
-				?>
-				<script>
-					window.addEventListener('sleek-ui-dialog-trigger-open', function (e) {
-						if (e.detail && e.detail.data) {
-							var dialog = e.detail.dialog;
-							var data = JSON.parse(e.detail.data);
-							var allLayouts = dialog.querySelectorAll('.acf-flexible-content > .values > .layout');
-							var clickedLayout = dialog.querySelector('.acf-flexible-content > .values > .layout[data-id="row-' + data.index + '"]');
-
-							if (allLayouts) {
-								allLayouts.forEach(function (el) {
-									el.classList.add('sleek-modules-inline-edit-hide');
-								});
-							}
-							if (clickedLayout) {
-								clickedLayout.classList.remove('sleek-modules-inline-edit-hide');
-							}
-						}
-					});
-				</script>
-				<?php
-			});
-		}
+			<nav class="sleek-modules-inline-edit">
+				<a href="#dialog-sleek-modules-inline-edit-<?php echo $where ?>-<?php echo $id ?>-<?php echo $index ?>" class="sleek-modules-inline-edit-module">
+					<?php _e('Edit Module', 'sleek') ?>
+				</a>
+			</nav>
+			<?php
+		}, 10, 5);
 	}
 });
