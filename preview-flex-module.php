5bb388bc87ea900d65ea6844c2a96722e6d19f6a
powerbuoy <powerbuoy@gmail.com>
2020-03-13 20:22:05 +0100

    Cleanup + noindex/nofollow preview

--- preview-flex-module.php
+++ preview-flex-module.php
@@ -4,6 +4,7 @@ namespace Sleek\Modules;
 
 # Return module at index from area/postId
 function get_flexible_module_by_area_index ($area, $postId, $index) {
+	# TODO: Sometimes the latest revision is actually _older_ than the post itself!?
 	$latestRev = wp_get_post_revisions($postId, ['numberposts' => 1]);
 	$postId = ($latestRev and count($latestRev)) ? array_key_first($latestRev) : $postId;
 #	$autosave = wp_get_post_autosave($postId, get_current_user_id());
@@ -26,27 +27,27 @@ function get_flexible_module_by_area_index ($area, $postId, $index) {
 add_action('init', function () {
 	if (get_theme_support('sleek/modules/module_preview')) {
 		add_filter('query_vars', function ($vars) {
-			$vars[] = 'sleek_modules_module_preview_area';
-			$vars[] = 'sleek_modules_module_preview_post_id';
-			$vars[] = 'sleek_modules_module_preview_index';
+			$vars[] = 'sleek_modules_preview_flex_module_area';
+			$vars[] = 'sleek_modules_preview_flex_module_post_id';
+			$vars[] = 'sleek_modules_preview_flex_module_index';
 
 			return $vars;
 		});
 
 		add_rewrite_rule(
-			'^sleek-modules-module-preview/([^/]+)/([^/]+)/([^/]+)/?$',
-			'index.php?sleek_modules_module_preview_area=$matches[1]&sleek_modules_module_preview_post_id=$matches[2]&sleek_modules_module_preview_index=$matches[3]',
+			'^__SLEEK__/modules/flex-module-preview/([^/]+)/([^/]+)/([^/]+)/?$',
+			'index.php?sleek_modules_preview_flex_module_area=$matches[1]&sleek_modules_preview_flex_module_post_id=$matches[2]&sleek_modules_preview_flex_module_index=$matches[3]',
 			'top'
 		);
 
 		add_action('template_redirect', function () {
 			global $wp_query;
 
-			if (isset($wp_query->query_vars['sleek_modules_module_preview_area'])) {
+			if (isset($wp_query->query_vars['sleek_modules_preview_flex_module_area'])) {
 				$module = get_flexible_module_by_area_index(
-					$wp_query->query_vars['sleek_modules_module_preview_area'],
-					(int) $wp_query->query_vars['sleek_modules_module_preview_post_id'],
-					(int) $wp_query->query_vars['sleek_modules_module_preview_index']
+					$wp_query->query_vars['sleek_modules_preview_flex_module_area'],
+					(int) $wp_query->query_vars['sleek_modules_preview_flex_module_post_id'],
+					(int) $wp_query->query_vars['sleek_modules_preview_flex_module_index']
 				);
 
 				if ($module and isset($module['acf_fc_layout'])) {
@@ -56,16 +57,18 @@ add_action('init', function () {
 					<html <?php language_attributes() ?> <?php body_class() ?>>
 						<head>
 							<?php wp_head() ?>
+							<meta name="robots" content="noindex,nofollow">
 						</head>
 						<body>
 							<?php
-							render($moduleName, $module);
+								render($moduleName, $module);
 							?>
 							<?php wp_footer() ?>
 						</body>
 					</html>
 					<?php
 				}
+
 				exit;
 			}
 		});
@@ -104,14 +107,11 @@ add_action('admin_head', function () {
 						var matches = data.match(/acf\[(.*?)\]/);
 						var area = matches[1];
 
+						// TODO: Create new revision when clicking button... HOW!?
 						previewButton.classList.add('acf-icon', '-picture', 'small', 'light', 'acf-js-tooltip', 'thickbox');
-						previewButton.href = '<?php echo home_url('/sleek-modules-module-preview/') ?>' + area + '/' + postId + '/' + index + '/?TB_iframe=true';
-
-					//	previewButton.href = '<?php echo get_preview_post_link($post->ID) ?>';
-					//	previewButton.target = 'wp-preview-<?php echo $post->ID ?>';
+						previewButton.href = '<?php echo home_url('/__SLEEK__/modules/flex-module-preview/') ?>' + area + '/' + postId + '/' + index + '/?TB_iframe=true';
 
 						previewButton.setAttribute('title', 'Preview');
-
 						controls.prepend(previewButton);
 					}
 				});
