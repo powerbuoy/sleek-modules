6119ac2cc3b459a25ca8478d0b948ece58d6bae6
Andreas Lagerkvist <powerbuoy@gmail.com>
2020-11-10 13:32:10 +0100

    Module icons

--- add-new-module-preview.php
+++ add-new-module-preview.php
@@ -9,7 +9,7 @@ function get_module_data () {
 
 	foreach (glob($path) as $file) {
 		$moduleName = basename(dirname($file));
-		$fullClassName = "Sleek\Modules\\" . \Sleek\Utils\convert_case($moduleName, 'pascal');
+		$fullClassName = "\\Sleek\Modules\\" . \Sleek\Utils\convert_case($moduleName, 'pascal');
 		$mod = new $fullClassName;
 		$templates = $mod->templates();
 		$meta = $mod->meta();
@@ -20,10 +20,44 @@ function get_module_data () {
 	return $moduleData;
 }
 
+#########################################
+# Insert icon into flexible content title
+add_filter('acf/fields/flexible_content/layout_title', function ($title, $field, $layout, $i) {
+	$moduleName = $layout['name'];
+	$fullClassName = "\\Sleek\Modules\\" . \Sleek\Utils\convert_case($moduleName, 'pascal');
+
+	if (class_exists($fullClassName)) {
+		$module = new $fullClassName;
+		$meta = $module->meta();
+
+		if ($meta['icon']) {
+			$title = '<figure class="sleek-module-icon"><img src="' . $meta['icon'] . '"></figure> ' . $title;
+		}
+	}
+
+	return $title;
+}, 11, 4);
+
+#################
+# Style the popup
 add_action('admin_head', function () {
 	if (get_theme_support('sleek/modules/add_new_module_preview')) {
 		?>
 		<style>
+			figure.sleek-module-icon {
+				width: 3rem;
+				height: 3rem;
+				margin: 0 0.5rem;
+				display: inline-block;
+				vertical-align: middle;
+			}
+
+			figure.sleek-module-icon img {
+				width: 100%;
+				height: 100%;
+				object-fit: contain;
+			}
+
 			div.acf-fc-popup {
 				--sleek-anmp-cols: 1;
 
@@ -138,6 +172,8 @@ add_action('admin_head', function () {
 	}
 });
 
+##################
+# Script the popup
 add_action('admin_footer', function () {
 	if (get_theme_support('sleek/modules/add_new_module_preview')) {
 		?>
