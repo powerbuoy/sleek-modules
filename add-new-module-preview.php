f63de735d415b89fc7b38bba044fa475d77c1ef9
powerbuoy <powerbuoy@gmail.com>
2021-11-17 13:16:50 +0100

    Use an external image instead of data:uri which is stripped by wp_kses

--- add-new-module-preview.php
+++ add-new-module-preview.php
@@ -200,7 +200,7 @@ add_action('admin_footer', function () {
 
 					// Add screenshot
 					var screenshot = document.createElement('figure');
-					var src = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48Y2lyY2xlIGN4PSIzMiIgY3k9IjMyIiByPSIzMiIgZmlsbD0iI0M0QzRDNCIvPjxwYXRoIGQ9Ik0yNy4zMjQyIDM4SDI1LjYyNzlMMTkuMTg1NSAyOC4xMzg3VjM4SDE3LjQ4OTNWMjUuMjAzMUgxOS4xODU1TDI1LjY0NTUgMzUuMTA4NFYyNS4yMDMxSDI3LjMyNDJWMzhaTTMwLjM5MTYgMzkuMDk4NkgyOC45OTQxTDM0LjMzNzkgMjUuMjAzMUgzNS43MjY2TDMwLjM5MTYgMzkuMDk4NlpNNDQuODA1NyAzNC42NjAySDM5LjQ0NDNMMzguMjQwMiAzOEgzNi41TDQxLjM4NjcgMjUuMjAzMUg0Mi44NjMzTDQ3Ljc1ODggMzhINDYuMDI3M0w0NC44MDU3IDM0LjY2MDJaTTM5Ljk1NDEgMzMuMjcxNUg0NC4zMDQ3TDQyLjEyNSAyNy4yODYxTDM5Ljk1NDEgMzMuMjcxNVoiIGZpbGw9ImJsYWNrIi8+PC9zdmc+';
+					var src = '<?php echo get_stylesheet_directory_uri() ?>/vendor/powerbuoy/sleek-modules/icon-fallback.svg';
 
 					if (moduleInfo.icon) {
 						src = moduleInfo.icon;
